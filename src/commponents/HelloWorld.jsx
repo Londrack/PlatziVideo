472b46d4986514481820d7316a6328759e1c66d0
Alejandro Mejía Escobar <alespacial@gmail.com>
2021-05-19 11:17:14 -0500

    Proyecto preparado

---
+++ src/commponents/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import '../assets/styles/App.scss'
+
+const HelloWorld = () => {
+    return (
+        <h1>Hola Mundo, Alejandro :)</h1>
+    )
+}
+
+export default HelloWorld
